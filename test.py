7f78d5c8a78f20aa00ac3649f8566babab41f596
vaseem-akram <akramv98@gmail.com>
2020-07-13 11:08:51 +0530

    just a program

---
+++ test.py
@@ -0,0 +1,14 @@
+
+for i in range(0,5):
+    n=1
+    for j in range(i):
+        print(n,end='')
+        n+=1
+    print()
+k=5
+for k in range(0,5):
+
+    for s in range(n):
+        print(k,end='')
+    n-=1
+    print()
