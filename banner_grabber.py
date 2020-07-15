65d36394d7547a86f8f2a6146f48ca2b7ba7fa35
vaseem-akram <akramv98@gmail.com>
2020-07-15 11:04:53 +0530

    Banner grabber program

---
+++ banner_grabber.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import socket
+from threading import *
+
+#Function to grab the service running on that particular port
+def retbanner(host,port):
+    try:
+        s=socket.socket()
+        s.connect((host,port))
+        banner=s.recv(5000)
+        print('for port {} is  '.format(port)+banner.decode('utf-8'))
+    except Exception as e:
+        print(' for {}'.format(port)+'   '+str(e))
+    
+
+
+
+
+def main():
+    
+    host=input('Enter the Host ip:  ')
+    print('the ip you entered'+host,end='')
+    hostname=socket.gethostbyaddr(host)
+    print(hostname)
+    try:
+        for port in range(20,50):
+            t=Thread(target=retbanner,args=(host,port))
+            t.start()
+            t.join()
+    except KeyboardInterrupt:
+        print('----------keyboard interrupt------------')
+    finally:
+        print('--------------scan completed-------------')
+main()
